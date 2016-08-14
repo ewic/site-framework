e0a6eeecb9200784ab6f9e979802ff6dc8d20202
Eric Zhang <ewicexclamationpoint@gmail.com>
2016-08-14 12:48:51 -0400

    Trying to put the views together.

--- site/js/app.js
+++ site/js/app.js
@@ -1,1 +1,47 @@
-define();
+require.config({
+	'baseUrl': '/',
+
+	'shim': {
+		'bootstrap': { 'deps': ['jquery'] },
+		'backbone': { 'deps': ['underscore', 'jquery'],
+						'exports': 'Backbone' }
+	},
+
+	'paths': {
+		'app': 'js/app',
+
+		//Vendor paths
+		'jquery': 'lib/js/jquery.min',
+		'underscore': 'lib/js/underscore-min',
+		'backbone': 'lib/js/backbone-min',
+		'mustache': 'lib/js/mustache.min',
+
+		'bootstrap': 'lib/js/bootstrap.min',
+	}
+});
+
+define(['jquery', 'underscore', 'backbone', 'mustache'], function($, _, Backbone, Mustache) {
+	var EditorView = Backbone.View.extend({
+		el: '#site-content',
+		tagName: 'div',
+		className: 'editor',
+
+		template: Mustache.render($('#tpl-editor').html()),
+
+		events: {
+			'keyup': 'changeText',
+		},
+
+		initialize: function() {
+			this.render();
+			console.log("editor view made");
+		},
+
+		changeText: function() {
+			console.log("keyup triggered");
+			$('#preview').html(this.$el.html());
+		},
+	});
+
+	var editor = new EditorView;
+});
