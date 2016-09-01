7e55243e169e79f651a17d910491b994ba8e8351
Eric Zhang <ewicexclamationpoint@gmail.com>
2016-09-01 17:04:12 -0400

    Removed bodyparser from deps

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ var config = require('./config.js');
 
 //ExpressJS server
 var express = require('express');
-var bodyParser = require("body-parser");
 var server = express();
-
-//Here we are configuring express to use body-parser as middle-ware.
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
 
 //Serve the static page
 server.use(express.static(config.site.root));
